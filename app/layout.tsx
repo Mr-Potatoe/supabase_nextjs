003333163714ea2e578f3bbb47213142f2446948
Mr-Potatoe <jaymeajarns@gmail.com>
2025-05-06 22:38:19 +0800

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
+  icons: {
+    icon: '/favicon.ico', // for default browsers
+  },
 };
 
 const geistSans = Geist({
